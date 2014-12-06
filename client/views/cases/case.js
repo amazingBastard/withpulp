404cdeebdfb6de83904e57c01866697725e1dca2
Damir <damir@withpulp.com>
2014-12-06 11:54:25 -0500

    polished case view styles

--- client/views/cases/case.js
+++ client/views/cases/case.js
@@ -10,10 +10,18 @@ Template.case.rendered = function () {
   reveal = 'reveal';
 
   $document.scroll(function() {
-    if ($document.scrollTop() >= 10) {
+    if ($document.scrollTop() >= 50) {
       $element.addClass(scrolled);
     } else {
       $element.removeClass(scrolled);
+    }
+  });
+
+  $document.scroll(function() {
+    if ($document.scrollTop() >= 400) {
+      $element.addClass(reveal);
+    } else {
+      $element.removeClass(reveal);
     }
   });
 }
