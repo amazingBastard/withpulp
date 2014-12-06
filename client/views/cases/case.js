cb7f0a7206a52225aea03df1757a09df3b3fdd3b
Damir <damir@withpulp.com>
2014-12-06 01:50:54 -0500

    polished case view styles

--- client/views/cases/case.js
+++ client/views/cases/case.js
@@ -1,5 +1,19 @@
 Template.case.rendered = function () {
   $('.topbar').hide();
+
   Session.set('menuActive', false);
   Session.set('menuShow', false);
+
+  var $document = $(document),
+  $element = $('.case.view.effect-push'),
+  scrolled = 'scrolled',
+  reveal = 'reveal';
+
+  $document.scroll(function() {
+    if ($document.scrollTop() >= 10) {
+      $element.addClass(scrolled);
+    } else {
+      $element.removeClass(scrolled);
+    }
+  });
 }
